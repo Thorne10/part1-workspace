c54943e3664a402b2f589889a6ee792101ddbcf5
Thorne10 <tamieka.horne@hotmail.com>
2022-09-06 15:13:32 -0700

    Added verifyInternetConnection. Static fields

--- Lab02.2/src/Television.java
+++ Lab02.2/src/Television.java
@@ -1,41 +1,73 @@
 public class Television {
 
+
+  public static final int MIN_VOLUME = 0;
+  public static final int MAX_VOLUME = 100;
+
+  private static int instanceCount = 0;
   private String brand;
   private int volume;
 
- public void turnOn() {
-   boolean isConnected = verifyInternetConnection();
-
-    System.out.println("The" + brand + "television is on, with a volume of" + volume);
+  public Television() {
+    instanceCount++;
 
   }
 
-  void turnOff() {
-    System.out.println("The" + brand + "television is off");
+  public Television(String brand) {
+    this();
+    setBrand(brand);
 
   }
 
-  public String getBrand() {
-    return brand;
+  public Television(String brand, int volume) {
+
+    this(brand);
+    setVolume(volume);
   }
 
-  public void setBrand(String brand) {
-    this.brand = brand;
+    public void turnOn () {
+      boolean isConnected = verifyInternetConnection();
+
+      System.out.println("The" + brand + "television is on, with a volume of" + volume);
+
+    }
+
+  public static int getInstanceCount() {
+    return instanceCount;
   }
 
-  public int getVolume() {
-    return volume;
-  }
+  void turnOff () {
+      System.out.println("The" + brand + "television is off");
 
-  public void setVolume(int volume) {
-    this.volume = volume;
-  }
-  private boolean verifyInternetConnection(){
-   return true;
-  }
+    }
 
-  public String toString() {
-    return  "Television: brand=samsung, + volume=32";
+    public String getBrand () {
+      return brand;
+    }
 
+    public void setBrand (String brand){
+      this.brand = brand;
+    }
+
+    public int getVolume () {
+      return volume;
+    }
+
+    public void setVolume ( int volume){
+      if (volume < MIN_VOLUME || volume > MAX_VOLUME) {
+        System.out.printf("%d is invalid; volume must be between %d and %d (inclusive)", volume, MIN_VOLUME, MAX_VOLUME);
+      } else {
+        this.volume = volume;
+      }
+
+    }
+
+    private boolean verifyInternetConnection () {
+      return true;
+    }
+
+    public String toString () {
+      return "Television: brand = " + brand + ",  volume=" + volume;
+
+    }
   }
-}
